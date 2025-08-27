108956708dbac7c9985b007156a6fd06a9bb9544
matias <matiasgfresnadillo@gmail.com>
2025-08-27 19:41:32 -0300

    #

---
+++ TP_3/app.js
@@ -0,0 +1,9 @@
+let productos = [
+  { id: 1, nombre: "Laptop", precio: 1200, stock: 10 },
+  { id: 2, nombre: "Mouse", precio: 25, stock: 50 },
+  { id: 3, nombre: "Teclado", precio: 45, stock: 30 },
+  { id: 4, nombre: "Monitor", precio: 300, stock: 15 },
+  { id: 5, nombre: "Auriculares", precio: 80, stock: 20 }
+];
+
+console.log(productos)
