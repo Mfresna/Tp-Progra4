a520044681636bae8b8d1e49ab2bfbddd21bb15e
matias <matiasgfresnadillo@gmail.com>
2025-08-27 21:28:20 -0300

    #

--- TP_3/app.js
+++ TP_3/app.js
@@ -1,4 +1,4 @@
-let productos = [
+const productos = [
   { id: 1, nombre: "Laptop", precio: 1200, stock: 10 },
   { id: 2, nombre: "Mouse", precio: 25, stock: 50 },
   { id: 3, nombre: "Teclado", precio: 45, stock: 30 },
@@ -6,4 +6,63 @@ let productos = [
   { id: 5, nombre: "Auriculares", precio: 80, stock: 20 }
 ];
 
-console.log(productos)
+/// EJERCICIO 1
+console.log('EJERCICIO 1');
+
+console.log(productos);
+
+productos.push({ id: 6, nombre: "Impresora", precio: 1600, stock: 4 });
+
+console.log("Lista Nueva");
+console.log(productos);
+
+productos.pop();
+
+console.log("Lista Eliminando ultimo elemento");
+console.log(productos);
+
+/// EJERCICIO 2
+console.log('\n EJERCICIO 2');
+
+    //funcion lambda
+let productosEnStock = productos.filter(p => p.stock > 10);
+    //funcion anonima
+let productosEnStock1 = productos.filter(function(p){ return p.stock > 10});
+
+console.log(productosEnStock1);
+
+/// EJERCICIO 3
+console.log('\n EJERCICIO 3');
+
+console.log(productos.find(p => p.nombre === "Laptop"));
+
+
+/// EJERCICIO 4
+console.log('\n EJERCICIO 4');
+let total = productos.reduce(function(a,producto){
+    return a + producto.precio
+},0);
+
+console.log('Total de costo: ' + total);
+console.log('Promedio del costo: ' + total/productos.length);
+
+
+/// EJERCICIO 5
+console.log('\n EJERCICIO 5');
+
+console.log('Hay productos con stock 0: ' + productos.some(p => p.stock === 0));
+console.log('Todos los productos valen mas que $100: ' + productos.every(p => p.precio > 100));
+
+
+/// EJERCICIO 6
+console.log('\n EJERCICIO 6');
+
+let clientes = [
+  {id: 1,nombre: "Ana Pérez",edad: 28,compras: ["Laptop", "Mouse", "Teclado"]},
+  {id: 2,nombre: "Carlos Gómez",edad: 35,compras: ["Smartphone", "Auriculares"]},
+  {id: 3,nombre: "Lucía Fernández",edad: 22,compras: ["Tablet", "Funda", "Cargador"]}
+];
+
+clientes.forEach(function(cliente){
+    console.log('Cliente: ' + cliente.nombre + ' - Cantidad de Compras: ' + cliente.compras.length);
+})
