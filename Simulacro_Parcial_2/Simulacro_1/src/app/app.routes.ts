95f386dcba6b09dd0fee83b0172e9178c508d6de
matias <matiasgfresnadillo@gmail.com>
2025-10-08 22:33:07 -0300

    #

--- Simulacro_Parcial_2/Simulacro_1/src/app/app.routes.ts
+++ Simulacro_Parcial_2/Simulacro_1/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { ProductoDetalle } from './pages/producto-detalle/producto-detalle';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home}
+];
--- Simulacro_Parcial_2/Simulacro_1/src/app/services/producto-service/producto-service.ts
+++ Simulacro_Parcial_2/Simulacro_1/src/app/services/producto-service/producto-service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@angular/core';
+import Producto from '../../models/producto';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ProductoService {
+
+  readonly API_URL = "http://localhost:3000/productos"
   
+  productos : Producto[]
+
+
+  constructor(private http: HttpClient){
+    this.productos = []
+  }
+
+  getProductos(){
+    return this.http.get<Producto[]>(this.API_URL)
+  }
+
+  getProducto(id : string){
+    return this.http.get<Producto>(`${this.API_URL}/id`)
+  }
+  
+  postProducto(p : Producto){
+    return this.http.post<Producto>(this.API_URL, p)
+  }
+
+  deleteProducto(id : string){
+    return this.http.delete<void>(`${this.API_URL}/id`)
+  }
+
+  updateProducto(p: Producto) {
+    return this.http.put<Producto>(`${this.API_URL}/${p.id}`, p);
+  }
+
 }
