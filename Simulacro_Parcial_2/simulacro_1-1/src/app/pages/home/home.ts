6241d494cd9663d5ed895194f6970644a23d337a
matias <matiasgfresnadillo@gmail.com>
2025-10-10 01:26:37 -0300

    #

--- Simulacro_Parcial_2/simulacro_1-1/src/app/pages/home/home.ts
+++ Simulacro_Parcial_2/simulacro_1-1/src/app/pages/home/home.ts
@@ -1,37 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Route } from '@angular/router';
 import { ProductosServicio } from '../../services/productos-servicio';
-import Producto from '../../models/producto';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home implements OnInit {
+export class Home implements OnInit{
 
-constructor(
-    public Service : ProductosServicio,
-    private router : Router){}
+  constructor(
+    public Service : ProductosServicio){}
 
   ngOnInit(): void {
     this.get()
   }
 
   get(){
-    this.Service.getProductos().subscribe({
-      next: (data) => { this.Service.productos = data},
-      error: (e) => { console.log(e)}
-    })
-  }
-
-  delete(id : string){
-    this.Service.deleteProducto(id).subscribe({
-      next: (data) => {this.get()},
-      error: (error) => { console.log(error) }
-    })
+  this.Service.getProductos().subscribe({
+    next: (data) => { this.Service.productos = data},
+    error: (e) => { console.log(e)}
+  })
   }
 
 }
--- Simulacro_Parcial_2/simulacro_1-1/src/app/services/productos-servicio.ts
+++ Simulacro_Parcial_2/simulacro_1-1/src/app/services/productos-servicio.ts
@@ -6,34 +6,17 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class ProductosServicio {
-  
   readonly API_URL = "http://localhost:3000/productos"
   
-    productos : Producto[]
+  productos : Producto[]
   
+  constructor(private http: HttpClient){
+    this.productos = []
+  }
   
-    constructor(private http: HttpClient){
-      this.productos = []
-    }
-  
-    getProductos(){
-      return this.http.get<Producto[]>(this.API_URL)
-    }
-  
-    getProducto(id : string){
-      return this.http.get<Producto>(`${this.API_URL}/id`)
-    }
-    
-    postProducto(p : Producto){
-      return this.http.post<Producto>(this.API_URL, p)
-    }
-  
-    deleteProducto(id : string){
-      return this.http.delete<void>(`${this.API_URL}/id`)
-    }
-  
-    updateProducto(p: Producto) {
-      return this.http.put<Producto>(`${this.API_URL}/${p.id}`, p);
-    }
+  getProductos(){
+    return this.http.get<Producto[]>(this.API_URL);
+  }
 
 }
+
