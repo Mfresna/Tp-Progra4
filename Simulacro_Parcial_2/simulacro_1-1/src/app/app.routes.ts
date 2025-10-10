5d89baabe1a2c74a60328df10bdb6c7b8fee3123
matias <matiasgfresnadillo@gmail.com>
2025-10-10 00:16:29 -0300

    #

--- Simulacro_Parcial_2/simulacro_1-1/src/app/app.routes.ts
+++ Simulacro_Parcial_2/simulacro_1-1/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { ProductoNuevo } from './pages/producto-nuevo/producto-nuevo';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'producto-form', component: ProductoNuevo}    
+];
--- Simulacro_Parcial_2/simulacro_1-1/src/app/pages/home/home.ts
+++ Simulacro_Parcial_2/simulacro_1-1/src/app/pages/home/home.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { ProductosServicio } from '../../services/productos-servicio';
+import Producto from '../../models/producto';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
+export class Home implements OnInit {
+
+constructor(
+    public Service : ProductosServicio,
+    private router : Router){}
+
+  ngOnInit(): void {
+    this.get()
+  }
+
+  get(){
+    this.Service.getProductos().subscribe({
+      next: (data) => { this.Service.productos = data},
+      error: (e) => { console.log(e)}
+    })
+  }
+
+  delete(id : string){
+    this.Service.deleteProducto(id).subscribe({
+      next: (data) => {this.get()},
+      error: (error) => { console.log(error) }
+    })
+  }
 
 }
