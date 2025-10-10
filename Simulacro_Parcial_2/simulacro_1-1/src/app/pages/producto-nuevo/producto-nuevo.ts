c0d81b5e2cdb69efba756b4eed6867fb4c75a01c
matias <matiasgfresnadillo@gmail.com>
2025-10-09 23:23:22 -0300

    #

---
+++ Simulacro_Parcial_2/simulacro_1-1/src/app/pages/producto-nuevo/producto-nuevo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-nuevo',
+  imports: [],
+  templateUrl: './producto-nuevo.html',
+  styleUrl: './producto-nuevo.css'
+})
+export class ProductoNuevo {
+
+}
